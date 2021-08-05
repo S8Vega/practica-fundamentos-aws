5fc473cc365db3b03897c4686384bda24a2a8fc7
Sebas <bsebastianvg18@gmail.com>
2021-08-05 08:47:39 -0500

    agregar PersonResponse

---
+++ src/main/java/com/pragma/practica/aws/PersonResponse.java
@@ -0,0 +1,14 @@
+package com.pragma.practica.aws;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PersonResponse {
+    private String message;
+}
